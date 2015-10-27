df0a0daf78b78afd457ffc03dc878580d578ded1
Jónas Arnþór Guðmundsson <Jonasag@clients-pool-1.nat.ru.is>
2015-10-27 19:44:12 +0000

    unitTest, test for winners

--- src/test/java/is/jatj/tictactoe/game/unitTest.java
+++ src/test/java/is/jatj/tictactoe/game/unitTest.java
@@ -54,4 +54,30 @@ public class unitTest{
 				}
 			}	
 		}
+
+		@Test
+		public void testWinnerH() {
+			game.makeMove(0, 0);
+			game.makeMove(1, 0);
+			game.makeMove(2, 0);
+			assertEquals(true, game.checkForWinner());
+			
+		}	
+		
+		@Test
+		public void testWinnerV() {
+			game.makeMove(0, 0);
+			game.makeMove(3, 0);
+			game.makeMove(6, 0);
+			assertEquals(true, game.checkForWinner());
+			
+		}
+		@Test
+		public void testWinnerA() {
+			game.makeMove(0, 0);
+			game.makeMove(4, 0);
+			game.makeMove(8, 0);
+			assertEquals(true, game.checkForWinner());
+			
+		}
 }
