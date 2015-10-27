37cfd4c2aebeebf4bf398465e30a8ae590fdf8bb
Jónas Arnþór Guðmundsson <Jonasag@clients-pool-1.nat.ru.is>
2015-10-27 17:35:13 +0000

    fyrsta unit test, profar move

--- src/test/java/is/jatj/tictactoe/game/unitTest.java
+++ src/test/java/is/jatj/tictactoe/game/unitTest.java
@@ -5,5 +5,16 @@ package test.java.is.jatj.tictactoe.game;
 
 public class unitTest{
 
-	
+	public static void main(String args[]) {
+	org.junit.runner.JUnitCore.main("test.java.is.jatj.tictactoe.game");
+	    }
+
+	    TicTacToe game = new TicTacToe();
+
+		@Test
+		public void testMakeMove() {
+			
+			game.makeMove(1, 0);
+			assertEquals('O', game.board[0][0]);
+		}
 }
