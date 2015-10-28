d21a44076af12aae9b114e3cfc2bc4d3fc7b5ee0
Kristjanjoelk <kristjank14@ru.is>
2015-10-28 02:45:14 +0000

    laga test klasa

--- src/test/java/is/jatj/tictactoe/game/TicTacToeTest.java
+++ src/test/java/is/jatj/tictactoe/game/TicTacToeTest.java
@@ -1,8 +1,6 @@
 package test.java.is.jatj.tictactoe.game;
 
-import edu.princeton.cs.algs4.In;
-import edu.princeton.cs.algs4.StdIn;
-import edu.princeton.cs.algs4.StdOut;
+import java.util.Scanner;
 
 public class TicTacToeTest {
 	public char[][] board;
@@ -38,12 +36,12 @@ public class TicTacToeTest {
 	{
 		if(move < 0 || move > 8)
 		{
-			StdOut.println("Out of bounds, try again");
+			System.out.println("Out of bounds, try again");
 			return false;
 		}
 		else if(board[move/3][move%3] == 'X' || board[move/3][move%3] == 'O')
 		{
-			StdOut.println("This move has already been made");
+			System.out.println("This move has already been made");
 			return false;
 		}
 		return true;
@@ -99,39 +97,43 @@ public class TicTacToeTest {
 	}
 	public void displayBoard()
 	{
-		StdOut.println();
 		for(int i = 0; i < 3; i++)
 		{
 			for(int j = 0; j < 3; j++)
 			{
-				StdOut.print(board[i][j] + " ");
+				System.out.print(board[i][j] + " ");
 			}
-			StdOut.println();
+			System.out.println();
 		}
 	};
+ 	public static int getInput()
+ 	{
+ 		Scanner reader = new Scanner(System.in);
+ 		int input = reader.nextInt() - 1;
+ 		return input;
+ 	}
 	public static void start(TicTacToeTest game)
 	{
-		In in = new In();
 		while(true)
 		{
-			StdOut.print("Player " + game.player[game.currentPlayer] + " select you're move: ");
-			int move = in.readInt() - 1;
+			game.displayBoard();
+			System.out.print("Player " + game.player[game.currentPlayer] + " select you're move: ");
+			int move = getInput();
 
 			game.makeMove(move, game.currentPlayer);
 			game.displayBoard();
 			// TODO: Check for win
 			game.currentPlayer = (game.currentPlayer + 1) % 2;
-			StdOut.println();
+			System.out.println();
 		}
 	}
 	public static void main(String[] args)
 	{
-		In in = new In();
 		TicTacToeTest game = new TicTacToeTest();
 		while(true)
 		{
 			start(game);
-			StdOut.println("Do you want to play another game? y/n ");
+			System.out.println("Do you want to play another game? y/n ");
 			// TODO: Check if continue
 			// TODO: score and switchplayer
 		}
