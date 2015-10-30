a813e0e48bc49dca85020556b4352d3d8cd691b7
Kristjanjoelk <kristjank14@ru.is>
2015-10-30 02:11:36 +0000

    webUI branch initial

---
+++ src/main/java/is/jatj/tictactoe/game/TicTacToeWebUI.java
@@ -0,0 +1,66 @@
+package main.java.is.jatj.tictactoe.game;
+
+import static spark.Spark.*;
+
+import spark.Request;
+import spark.Response;
+import spark.Route;
+public class TicTacToeWebUI {
+	TicTacToeController gameController;
+	
+	public TicTacToeWebUI()
+	{
+		gameController = new TicTacToeController();
+	}
+	public void start() {
+		displayBoard();
+		get("/move/:param", new Route() {
+        	public Object handle(Request request, Response response) {
+                    int move = Integer.parseInt(request.params(":param"));
+                    if(!gameController.isMoveLegal(move)) {
+                    	return body("<p>do something</p>");
+                    }
+                    gameController.makeMove(move);
+                    if(gameController.checkForWinner()) {
+                    	return body("<p>do something</p>");
+                    }
+                    gameController.updatePlayer();
+                    return body(displayBoard());
+        	}
+	});
+	}
+	public String body(String output)
+	{
+		 String head = "<!doctype html><html><head><style></style><title>JATJ - TicTactoe</title></head><body>";
+		 String end = "</body></html>";
+		 String body = head + output + end; 
+		 return body;
+	}
+	public String displayBoard() {
+		String boardOutput = "";
+		char[][] board = gameController.getBoard();
+		for (int i = 0; i < 3; i++) {
+			for(int j = 0; j < 3; j++) {
+				boardOutput += "<a href='/move/" + i + "'>" + board[i][j] + "</a>";
+			}
+		}
+		return boardOutput;
+	}
+	public static void main(String[] args) {
+		TicTacToeWebUI gameUI = new TicTacToeWebUI();
+		port(getHerokuAssignedPort());
+	    
+		gameUI.start();
+		
+		
+		
+	}
+	
+	static int getHerokuAssignedPort() {
+	    ProcessBuilder processBuilder = new ProcessBuilder();
+	    if (processBuilder.environment().get("PORT") != null) {
+	    	return Integer.parseInt(processBuilder.environment().get("PORT"));
+	    }
+	return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+	}
+}
